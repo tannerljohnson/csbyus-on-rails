659cbdf6995e3ad428401fb148164ad4585ec275
Tanner Johnson <tlj21@duke.edu>
2018-08-13 21:41:46 -0600

    initial commit

---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,17 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Segment } from "semantic-ui-react"
+class HelloWorld extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Greeting: {this.props.greeting}
+      </React.Fragment>
+    );
+  }
+}
+
+HelloWorld.propTypes = {
+  greeting: PropTypes.string
+};
+export default HelloWorld
