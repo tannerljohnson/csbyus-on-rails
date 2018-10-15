5b5a2330958b841af2c1f7321b94135918313785
cady <candyzhouziqing@hotmail.com>
2018-10-15 23:40:30 +0800

    url fixed

--- app/javascript/components/CourseCard.js
+++ app/javascript/components/CourseCard.js
@@ -3,10 +3,8 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
@@ -40,7 +38,7 @@ class CourseCard extends React.Component {
                       className={classes.media}
                       height="140"
                       // FIXME: fix the url
-                      image="app/images/tanner.jpg"
+                      image="http://csbyus.org/wp-content/uploads/2018/03/CSbyUs_logo.png"
                   />
                   <CardContent>
                       <Typography gutterBottom variant="h5" component="h2">
