4b545ea146c35aff668cd753a2f392104033eeee
root <candyzhouziqing@hotmail.com>
2018-10-25 11:37:34 +0800

    fix minor syntax mistakes

--- app/javascript/components/Blog.js
+++ app/javascript/components/Blog.js
@@ -2,20 +2,25 @@ import React from "react"
 import PropTypes from "prop-types"
 import Post from "./Post"
 class Blog extends React.Component {
-  const data = [{title:"Title1", author:"Eliza", content:"Post1", timestamp:"Friday, October 19th at 2:34 pm"}, {title:"Title2", author:"Megan", content:"Post2", timestamp:"Friday, October 19th at 2:34 pm"}. {title:"Title3", author:"Tanner", content:"Post3", timestamp:"Friday, October 19th at 2:34 pm"}]
+
   render () {
-    var postNodes = this.props.data.map(function(post)) {
-    //declare nodes
-    }
+      const data =
+          [{title:"Title1", author:"Eliza",
+              content:"Post1", timestamp:"Friday, October 19th at 2:34 pm"},
+          {title:"Title2", author:"Megan",
+              content:"Post2", timestamp:"Friday, October 19th at 2:34 pm"},
+          {title:"Title3", author:"Tanner",
+              content:"Post3", timestamp:"Friday, October 19th at 2:34 pm"}];
+
     return (
-      <React.Fragment>
+        //TODO: see if this works
       	<div>
       		Welcome to our blog!
-          //use function .map()
-          {postNodes}
+            {data.map(post => (
+                <Post title = {post.title} author = {post.author} content = {post.content} timestamp = {post.timestamp}/>
+            ))}
       	</div>
-      	<Post title = "First post! " author = "Eliza " content = "I love CSbyUS! " timestamp = "Friday, October 19th at 2:19 pm "/>
-      </React.Fragment>
+
     );
   }
 }
