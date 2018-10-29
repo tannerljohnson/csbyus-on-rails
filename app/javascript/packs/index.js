60692d4870464661e87030c9e0dbb9e47126729c
cady <candyzhouziqing@hotmail.com>
2018-10-29 21:45:55 +0800

    revert previous changes before merging

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from '../components/App'
-import CourseOverview from '../components/CourseOverview'
-import Sidebar from '../components/Sidebar'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Router>
-      <Route path="/" component={CourseOverview} />
+      <Route path="/" component={App} />
     </Router>,
   document.body.appendChild(document.createElement('div'))
   )
