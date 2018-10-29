077843303913ac90244be5663b33171396e43a85
cady <candyzhouziqing@hotmail.com>
2018-10-29 22:21:47 +0800

    fix progressbar bug

--- app/javascript/components/ProgressBar.js
+++ app/javascript/components/ProgressBar.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {LinearProgress} from '@material-ui/core/LinearProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
 
 class ProgressBar extends React.Component {
   render() {
+      const { classes } = this.props;
     const progress = 70;
-
     return (
-      <div>
-        <LinearProgress variant="determinate" value ={progress}/>
-        The progress is {progress}%
+      <div className={ classes.root }>
+        <LinearProgress variant="determinate" value ={ progress }/>
+        The progress is { progress }%
       </div>
     );
   }
 }
 
 ProgressBar.propTypes = {
-  course: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
-
-export default ProgressBar
+export default withStyles(styles)(ProgressBar);
