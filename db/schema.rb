aa7916618561a1bf55f434322ea47193c29b6460
Tanner Johnson <tlj21@duke.edu>
2018-11-14 13:48:42 +0100

    update for postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181016150422) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "author"
     t.string "text"
