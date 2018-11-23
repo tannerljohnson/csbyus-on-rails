bd0c872ee667ec8753f1ae9ca81215ae029c10a3
CadyZQ <Candy45231911>
2018-11-22 23:54:33 -0500

    Reformat

--- app/javascript/components/HeaderAppBar.js
+++ app/javascript/components/HeaderAppBar.js
@@ -2,49 +2,46 @@ import React from "react"
 import PropTypes from "prop-types"
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import MainMenu from './MainMenu';
 import Typography from '@material-ui/core/Typography';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { withStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import { withRouter } from 'react-router-dom';
 
 
 const styles = theme => ({
-  appBar: {
-    position: 'fixed',
-    margin: '0 auto',
-    top: 0,
-    padding: '0%',
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
+    appBar: {
+        position: 'fixed',
+        paddingTop: '0%',
+    },
+
+    menuButton: {
+        marginLeft: 0,
+        marginRight: 200,
+    },
 });
 
-const theme = createMuiTheme();
 
 class HeaderAppBar extends React.Component {
 
-  render () {
-    const { classes } = this.props;
+    render() {
+        const {classes} = this.props;
 
-    return (
-      <AppBar className={classes.appBar}>
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MainMenu />
-          </IconButton>
-          <Typography variant="title" color="inherit" noWrap>
-            CSbyUs
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    );
-  }
+        return (
+            <AppBar className={classes.appBar}>
+                <Toolbar>
+                    <MainMenu className={classes.menuButton}/>
+                    <Typography variant="title" color="inherit" noWrap>
+                        CSbyUs
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        );
+    }
 }
 
+HeaderAppBar.propTypes = {
+    classes: PropTypes.object.isRequired
+};
 const HeaderWithRouter = withRouter(HeaderAppBar);
 export default withStyles(styles)(HeaderWithRouter);
