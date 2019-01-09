9a3aed96b2b81e8cd9d4c1e6a295f3b22896d6b9
Tanner Johnson <tlj21@duke.edu>
2019-01-09 22:14:07 +0100

    bug fixes again

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
 
 
 module ReactRailsExampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.initialize_on_precompile = false
+
     config.load_defaults 5.1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
