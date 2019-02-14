7c5e2ce509a8d3b31eade32edb37df3dabf91bf4
Tanner Johnson <tlj21@duke.edu>
2019-02-14 20:01:35 +0100

    merged megans app bar and fixed several formatting issues like about us responsive design and a weird floating app bar in currhub and about

--- app/javascript/components/AboutLogo.js
+++ app/javascript/components/AboutLogo.js
@@ -3,15 +3,14 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import HomeLogoLayout from './HomeLogoLayout';
-import Logo from '../../assets/images/csbyuslogo.png';
-
+import Background from '../../assets/images/AboutUs.png';
 
 const backgroundImage =
   'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400&q=80';
 
 const styles = theme => ({
   background: {
-    backgroundImage: `url(${Logo})`,
+    backgroundImage: `url(${Background})`,
     backgroundColor: theme.palette.secondary.light, // Average color of the background image.
     backgroundPosition: 'center',
   },
@@ -19,8 +18,8 @@ const styles = theme => ({
     fontFamily: theme.Typography.fontFamily[0],
     marginTop: theme.spacing.unit * 20,
     [theme.breakpoints.up('sm')]: {
-      marginTop: theme.spacing.unit * 70,
-      marginRight: theme.spacing.unit * 50,
+      marginTop: theme.spacing.unit * 35,
+      marginRight: theme.spacing.unit * 10,
       marginLeft: theme.spacing.unit * 10,
       marginBottom: theme.spacing.unit * 10,
     },
@@ -38,22 +37,22 @@ const styles = theme => ({
 });
 
 
-function HomeLogo(props) {
+function AboutLogo(props) {
   const { classes } = props;
 
   return (
     <HomeLogoLayout backgroundClassName={classes.background}>
       {/* Increase the network loading priority of the background image. */}
       <img style={{ display: 'none' }} src={backgroundImage} alt="" />
-      <Typography color="textSecondary" align="center" variant="h2" marked="center" className={classes.h2} >
-        It's time to make learning computer science easier
+      <Typography color="inherit" align="center" variant="h1" marked="center" className={classes.h2} >
+        About <b>Us</b>
       </Typography>
     </HomeLogoLayout>
   );
 }
 
-HomeLogo.propTypes = {
+AboutLogo.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(HomeLogo);
+export default withStyles(styles)(AboutLogo);
