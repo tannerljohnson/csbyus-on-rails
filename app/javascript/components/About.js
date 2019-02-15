ba6a3de6d64172a82d6c05d20f6674b163cb5f91
Eliza Paradise <ecp25@duke.edu>
2019-02-15 18:50:13 -0500

    working on displaying cards

--- app/javascript/components/About.js
+++ app/javascript/components/About.js
@@ -5,6 +5,8 @@ import Typography from '@material-ui/core/Typography';
 import Background from '../../assets/images/AboutUs.png';
 import InfoSegment from "./InfoSegment"
 import aboutComponentStyles from "../../assets/javascripts/jss/components/aboutComponentStyles.js";
+import TeamCard from "./TeamCard";
+import Card from '@material-ui/core/Card';
 
 
 class About extends React.Component {
@@ -12,6 +14,15 @@ class About extends React.Component {
   render () {
 
     const { classes } = this.props;
+     const data =
+          [
+          {name:"Curriculum Research and Development", 
+              content:"The Curriculum Research & Development (CRD) team uses design-based research to create and test inclusive CS curricula. Currently, the curriculum development mini-team is designing a curriculum to self-empower students to take control of their data and act against algorithmic bias. The curriculum research mini-team is collecting data on the efficacy of our Mobile Citizens curriculum, using these insights to inform future curriculum iterations."},
+          {name:"Teaching Team", 
+              content:"The Teaching Team (TT) is a community of mentors invested in building each other to become more excellent, caring mentors for our middle school students at StudentU and Durham Salvation Army Boys & Girls Club. Most members on TT are also involved in TRD and CRD so they use their work within classrooms to build better, respectively, technology and curriculum to improve the learning experiences of our students."},
+          {name:"Technology Research and Development",
+              content:"The Tech Research & Development team (TRD) created the web application you're currently using. TRD works to scale the CSbyUs mission beyond our classrooms in Durham. CurriculaHub, alongside other resources on this site, is a one-stop shop for fellow educators to explore, adapt, discuss, and teach our CSbyUs curricula and others. In a nutshell, TRD uses technology to make teaching digital era education easier for teachers."}
+            ];
 
     return (
       <React.Fragment> 
@@ -26,27 +37,15 @@ class About extends React.Component {
         <div className={classes.layout}>
           <InfoSegment />
         </div>
+
+        const cards = data.map((card) =>
+          <TeamCard name= {card.name} 
+                content = {card.content}/>
+
       </React.Fragment>
+
     );
 
-    //to do: where to place this so it is below other info?
-    const data =
-          [
-          {name:"Curriculum Research and Development", 
-              content:"CRD description",
-              url: 'to include'},
-          {title:"Teaching Team", 
-              content:"The Teaching Team (TT) is a community of mentors invested in building each other to become more excellent, caring mentors for our middle school students at StudentU and Durham Salvation Army Boys & Girls Club. Most members on TT are also involved in TRD and CRD so they use their work within classrooms to build better, respectively, technology and curriculum to improve the learning experiences of our students."
-              url: 'to include'},
-          {title:"Technology Research and Development",
-              content:"The Tech Research & Development team (TRD) created the web application you're currently using. TRD works to scale the CSbyUs mission beyond our classrooms in Durham. CurriculaHub, alongside other resources on this site, is a one-stop shop for fellow educators to explore, adapt, discuss, and teach our CSbyUs curricula and others. In a nutshell, TRD uses technology to make teaching digital era education easier for teachers.",
-              url: 'to include'}
-            ];
-
-      const cards = data.map((card) =>
-          <TeamCard name= {card.name} 
-                content = {card.content} url = {card.url}/>
-      );
 
   }
 }
