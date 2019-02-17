36d751addbb640e46c32043db8f1ff0248842e22
Eliza Paradise <ecp25@duke.edu>
2019-02-17 13:49:52 -0500

    cards properly laid out, in infosegment

--- app/javascript/components/About.js
+++ app/javascript/components/About.js
@@ -12,16 +12,7 @@ class About extends React.Component {
   render () {
 
     const { classes } = this.props;
-     {/*const data =
-          [
-          {name:"Curriculum Research and Development",
-              content:"The Curriculum Research & Development (CRD) team uses design-based research to create and test inclusive CS curricula. Currently, the curriculum development mini-team is designing a curriculum to self-empower students to take control of their data and act against algorithmic bias. The curriculum research mini-team is collecting data on the efficacy of our Mobile Citizens curriculum, using these insights to inform future curriculum iterations."},
-          {name:"Teaching Team",
-              content:"The Teaching Team (TT) is a community of mentors invested in building each other to become more excellent, caring mentors for our middle school students at StudentU and Durham Salvation Army Boys & Girls Club. Most members on TT are also involved in TRD and CRD so they use their work within classrooms to build better, respectively, technology and curriculum to improve the learning experiences of our students."},
-          {name:"Technology Research and Development",
-              content:"The Tech Research & Development team (TRD) created the web application you're currently using. TRD works to scale the CSbyUs mission beyond our classrooms in Durham. CurriculaHub, alongside other resources on this site, is a one-stop shop for fellow educators to explore, adapt, discuss, and teach our CSbyUs curricula and others. In a nutshell, TRD uses technology to make teaching digital era education easier for teachers."}
-            ]; */}
-
+    
     return (
       <React.Fragment>
       {/* Use CssBaseline to fix header bar spacing */}
@@ -29,13 +20,6 @@ class About extends React.Component {
         {/* Big picture */}
         <AboutLogo />
         <InfoSegment />
-        {/*<Grid container spacing = {24}>
-          {data.map(card => (
-              <Grid item key={card.name}>
-                  <TeamCard name={card.name} content={card.content}/>
-              </Grid> 
-          ))} 
-      </Grid> */}
       </React.Fragment>
     );
   }
